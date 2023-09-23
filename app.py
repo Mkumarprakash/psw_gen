d471028cc0238ffb3bf9873dd66926ff0761ff53
Prakash kumar Mallick <103797155+Mkumarprakash@users.noreply.github.com>
2023-09-23 11:52:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from generator import Gen_password
 
 app = Flask(__name__)
+@app.route('/')
 def index():
     password = Gen_password()
     return render_template('index.html', password=password)
